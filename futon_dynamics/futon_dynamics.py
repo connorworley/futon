485d64790f5274b210677cf5888d9ffb33df8e33
Connor Worley <cbworley@yelp.com>
2019-11-09 19:56:34 -0800

    it works

--- futon_dynamics/futon_dynamics.py
+++ futon_dynamics/futon_dynamics.py
@@ -8,20 +8,20 @@ import kubernetes
 from envoy.api.v2.cds_pb2 import Cluster
 from envoy.api.v2.core.address_pb2 import Address
 from envoy.api.v2.core.address_pb2 import SocketAddress
+from envoy.api.v2.discovery_pb2 import DiscoveryResponse
+from envoy.api.v2.discovery_pb2 import DiscoveryRequest
 from envoy.api.v2.eds_pb2 import ClusterLoadAssignment
 from envoy.api.v2.endpoint.endpoint_pb2 import Endpoint
 from envoy.api.v2.endpoint.endpoint_pb2 import LbEndpoint
 from envoy.api.v2.endpoint.endpoint_pb2 import LocalityLbEndpoints
 from flask import Flask
-from google.protobuf.json_format import MessageToDict
+from flask import request
+from flask import Response
+from google.protobuf.any_pb2 import Any
+from google.protobuf.duration_pb2 import Duration
+from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import Parse
 from waitress import serve
-
-
-class ClusterEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Cluster):
-            return MessageToDict(obj)
-        return json.JSONEncoder.default(self, obj)
 
 
 @lru_cache(maxsize=1)
@@ -33,8 +33,10 @@ def k8s_client():
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
+    @app.route("/v2/discovery:clusters", methods=["POST"])
     def get_clusters():
+        discovery_request = Parse(request.data, DiscoveryRequest())
+
         clusters = []
 
         services = k8s_client().list_service_for_all_namespaces(watch=False)
@@ -43,6 +45,8 @@ def create_app():
 
         for service in services.items:
             if service.spec.type != "NodePort":
+                continue
+            if len(discovery_request.resource_names) > 0 and service.metadata.name not in discovery_request.resource_names:
                 continue
 
             node_ports = [port.node_port for port in service.spec.ports]
@@ -84,6 +88,7 @@ def create_app():
             clusters.append(
                 Cluster(
                     name=service.metadata.name,
+                    connect_timeout=Duration(seconds=5),
                     type=Cluster.STATIC,
                     load_assignment=ClusterLoadAssignment(
                         cluster_name=service.metadata.name,
@@ -92,14 +97,23 @@ def create_app():
                 ),
             )
 
-        return json.dumps(clusters, cls=ClusterEncoder, indent=4)
+        response = DiscoveryResponse()
+        for cluster in clusters:
+            cluster_as_any = Any()
+            cluster_as_any.Pack(cluster)
+            response.resources.append(cluster_as_any)
+
+        return Response(
+            MessageToJson(response),
+            mimetype='application/json',
+        )
 
     return app
 
 
 def main(_argv):
     logger = logging.getLogger("waitress")
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     serve(create_app(), listen="*:8888")
 
 
